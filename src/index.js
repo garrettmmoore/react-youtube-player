38fed9e2f4ec6c8bf39c59f8c39b977ccb800b56
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 17:44:47 -0800

    Add youtube-api-search npm package. Fix process.env to work with create-react-app by adding REACT_APP.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
-import SearchBar from './components/search_bar';
+import SearchBar from "./components/search_bar";
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
+	console.log(data);
+});
 
 const App = () => {
-    return (
-    <div>
-        <SearchBar/>
-    </div>
-    );
-}
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
