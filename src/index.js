d47fa3dda04faf9e93bd15ad6825e437edd62c78
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 17:47:41 -0800

    Change functional App component into class component.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 
@@ -10,12 +10,14 @@ YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
 	console.log(data);
 });
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
