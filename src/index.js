fa3efeff7a9c9c3366b57a985df893fdf01f42a2
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 16:16:10 -0800

    Created Search Bar component.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar/>
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
