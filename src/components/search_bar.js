8268c2cdc6a6d6edf02d8bfc2a99e082fd435d90
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 21:11:11 -0800

    Add CSS styling to SearchBar, VideoDetail, and VideoListItem components.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,15 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { term: "Starting Value" };
+		this.state = { term: '' };
 	}
 
 	render() {
 		return (
-			<div>
-                
-				<input onChange={event => this.setState({ term: event.target.value })} />
+			<div className="search-bar">
+                <input
+                value={this.state.term}
+                onChange={event => this.setState({ term: event.target.value })} />
 			</div>
 		);
 	}
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -6,7 +6,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
     const imageUrl = video.snippet.thumbnails.default.url;
     const title = video.snippet.title;
     return (
-    <li onClick={() => onVideoSelect(video)}className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="video-list media">
             <div className="media-left">
                 <img className="media-object" src={imageUrl} />
