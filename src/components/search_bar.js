023fa2f2d23a0e957f33b29fe8b63d124d115ef3
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 23:13:57 -0800

    Clean up code.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,22 +4,20 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { term: '' };
+		this.state = { term: "" };
 	}
 
 	render() {
 		return (
 			<div className="search-bar">
-                <input
-                value={this.state.term}
-                onChange={event => this.onInputChange(event.target.value )} />
+				<input value={this.state.term} onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
-    }
-    onInputChange(term) {
-        this.setState({term});
-        this.props.onSearchTermChange(term);
-    }
+	}
+	onInputChange(term) {
+		this.setState({ term });
+		this.props.onSearchTermChange(term);
+	}
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class App extends Component {
 			videos: [],
 			selectedVideo: null
 		};
+
 		this.videoSearch("surfboards");
 	}
 
@@ -29,7 +30,8 @@ class App extends Component {
 
 	render() {
 		const videoSearch = _.debounce(term => {
-			this.videoSearch(term)}, 300);
+			this.videoSearch(term);
+		}, 300);
 
 		return (
 			<div>
