fb84f50d9ccf690db505d3b0c4a77c7683b5a951
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 20:55:04 -0800

    Pass down onVideoSelect callback from app to videoList to videoListItem.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
     // const video = props.video;
     console.log(video);
     const imageUrl = video.snippet.thumbnails.default.url;
     const title = video.snippet.title;
     return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)}className="list-group-item">
         <div className="video-list media">
             <div className="media-left">
                 <img className="media-object" src={imageUrl} />
