4cc2142e58c9595d2082d8f2db666f925550419a
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 21:21:47 -0800

    In Index.js: -Refactor YT search into its own method. -Take VideoSearch method and pass down into searchBar under the property onSearchTermChange.
    In search_bar.js:
    -Refactor onChange event to call onInputChange whenever the content of the search changes

--- src/index.js
+++ src/index.js
@@ -16,20 +16,23 @@ class App extends Component {
              videos: [],
              selectedVideo: null
             };
+        this.videoSearch('surfboards');
 
-        YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
-            //this.setState({videos:videos});
         });
     }
 
 	render() {
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                 onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
