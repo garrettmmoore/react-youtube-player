17148eac53efc43a01b6866e416cbc28a174fda1
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 21:33:45 -0800

    Install Lodash package. Use Lodash function .debounce as a way to throttle user input.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
@@ -6,38 +7,38 @@ import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-             videos: [],
-             selectedVideo: null
-            };
-        this.videoSearch('surfboards');
+	constructor(props) {
+		super(props);
 
-    }
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch("surfboards");
+	}
 
-    videoSearch(term) {
-        YTSearch({ key: API_KEY, term: term }, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term }, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
 
 	render() {
+		const videoSearch = _.debounce(term => {
+			this.videoSearch(term)}, 300);
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-                videos={this.state.videos}
-                />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
