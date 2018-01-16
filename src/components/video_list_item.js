9342f04c7b2cdca5f0125bd6204287ff50486371
Garrett Moore <garrettmarkmoore@gmail.com>
2018-01-15 18:59:13 -0800

    Add React Bootstrap CDN. Create VideoList and VideoListItem components.

---
+++ src/components/video_list_item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoListItem = (prop) => {
+    return <li>Video</li>
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -15,7 +16,7 @@ class App extends Component {
         this.state = { videos: [] };
 
         YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
-            this.setState({ videos});
+            this.setState({ videos });
             //this.setState({videos:videos});
         });
     }
@@ -24,6 +25,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+                <VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
