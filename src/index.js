1043266e51af647d00e37c2a24bd146490a29202
Garrett Moore <garrettmarkmoore@gmail.com>
2020-09-05 01:31:40 -0700

    fix: remove video selection destructuring

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
+import { debounce } from 'lodash/function';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
-const API_KEY = process.env.REACT_APP_API_KEY;
+import searchYouTube from './utils/searchYouTube';
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const App = () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
   const videoSearch = (term) => {
-    YTSearch({ key: API_KEY, term: term }, (videos) => {
+    searchYouTube({ key: API_KEY, term: term }, (videos) => {
       setVideos(videos);
       setSelectedVideo(videos[0]);
     });
   };
 
-  const onSearchTermChange = _.debounce((term) => {
+  const onSearchTermChange = debounce((term) => {
     videoSearch(term);
-  }, 300);
+  }, 600);
 
   return (
     <div>
       <SearchBar onSearchTermChange={onSearchTermChange} />
       <VideoDetail video={selectedVideo} />
       <VideoList
-        onVideoSelect={(selectedVideo) => setSelectedVideo({ selectedVideo })}
+        onVideoSelect={(selectedVideo) => setSelectedVideo(selectedVideo)}
         videos={videos}
       />
     </div>
